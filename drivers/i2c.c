c08f1974272ab21df01ae645b1b4d8400f76d8f8
오병준 <mail@luftaquila.io>
2023-11-07 18:43:03 +0900

    add: i2c_transfer for combined i2c comm

--- drivers/i2c.c
+++ drivers/i2c.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <string.h>
 
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+
+#include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 
 #include "drivers/i2c.h"
@@ -13,7 +14,7 @@
  * References
  *
  * open(2), close(2), read(2), write(2), ioctl(2), errno(3)
- * https://elinux.org/Interfacing_with_I2C_Devices
+ * https://www.kernel.org/doc/Documentation/i2c/dev-interface
  */
 
 int i2c_register(const char *bus, int *fd) {
@@ -37,7 +38,11 @@ int i2c_unregister(int fd) {
 }
 
 int i2c_write(int fd, int addr, uint8_t *buf, int len) {
-  ioctl(fd, I2C_SLAVE, addr << 1);
+  int ret = ioctl(fd, I2C_SLAVE, addr << 1);
+
+  if (ret < 0) {
+    return -errno;
+  }
 
   if (write(fd, buf, len) != len) {
     return -ETIMEDOUT;
@@ -47,10 +52,42 @@ int i2c_write(int fd, int addr, uint8_t *buf, int len) {
 }
 
 int i2c_read(int fd, int addr, uint8_t *buf, int len) {
-  ioctl(fd, I2C_SLAVE, (addr << 1) | 0x1);
+  int ret = ioctl(fd, I2C_SLAVE, (addr << 1) | 0x1);
+
+  if (ret < 0) {
+    return -errno;
+  }
 
   if (read(fd, buf, len) != len) {
     return -ETIMEDOUT;
+  }
+
+  return 0;
+}
+
+int i2c_transfer(int fd, int addr, i2c_buf *buf) {
+  struct i2c_rdwr_ioctl_data payload;
+  struct i2c_msg msg[2];
+
+  payload.msgs = msg;
+  payload.nmsgs = 2;
+
+  /* write transfer */
+  msg[0].addr = addr;
+  msg[0].flags = 0;
+  msg[0].len = buf->tx_len;
+  msg[0].buf = buf->tx_buf;
+
+  /* read transfer without STOP */
+  msg[1].addr = addr;
+  msg[1].flags = I2C_M_RD;
+  msg[1].len = buf->rx_len;
+  msg[1].buf = buf->rx_buf;
+
+  int ret = ioctl(fd, I2C_RDWR, &payload);
+
+  if (ret < 0) {
+    return -errno;
   }
 
   return 0;
--- includes/drivers/i2c.h
+++ includes/drivers/i2c.h
@@ -8,6 +8,28 @@
 #define I2C1 "/dev/i2c-1"
 
 /**
+ * @struct i2c_buf
+ * I2C read/write buffer
+ *
+ * @var   i2c_buf::tx_buf
+ *          A transmit buffer of I2C transfer
+ * @var   i2c_buf::tx_len
+ *          A length of the transmit buffer
+ * @var   i2c_buf::rx_buf
+ *          A receive buffer of I2C transfer
+ * @var   i2c_buf::rx_len
+ *          A length of the receive buffer
+ */
+typedef struct {
+  uint8_t *tx_buf;
+  int tx_len;
+
+  uint8_t *rx_buf;
+  int rx_len;
+} i2c_buf;
+
+
+/**
  * Register a I2C bus
  *
  * @param[in]   bus   The i2c device
@@ -53,5 +75,17 @@ int i2c_write(int fd, int addr, uint8_t *buf, int len);
  *            fail: `-errno`
  */
 int i2c_read(int fd, int addr, uint8_t *buf, int len);
+
+/**
+ * Perform a write-and-read transfer without STOP through a I2C bus
+ *
+ * @param[in]     fd    The file descriptor of the device to read
+ * @param[in]     addr  The address of the target I2C slave
+ * @param[inout]  buf   The r/w buffer for combined transfer
+ *
+ * @return    success: 0
+ *            fail: `-errno`
+ */
+int i2c_transfer(int fd, int addr, i2c_buf *buf);
 
 #endif /* __DRIVER_I2C_H */
