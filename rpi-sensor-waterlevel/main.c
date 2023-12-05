328065ca68f1d2a03d550663f50294f3438cbdc5
오병준 <mail@luftaquila.io>
2023-12-05 14:03:18 +0900

    add: led display update

--- rpi-sensor-waterlevel/main.c
+++ rpi-sensor-waterlevel/main.c
@@ -12,17 +12,20 @@
 #include "drivers/i2c.h"
 #include "drivers/pcf8561.h"
 #include "drivers/adxl345.h"
+#include "drivers/ledmatrix.h"
 
 /* global variables */
 int i2c1;
 pthread_mutex_t lock;
 payload_t payload_adc;
 payload_t payload_accel;
+int display_data[4];
 
 /* function prototypes */
 void *thread_job_adc(void *arg);
 void *thread_job_accel(void *arg);
 void *thread_job_socket(void *arg);
+void *thread_job_display(void *arg);
 
 /**************************************************************************
  * main function
@@ -38,21 +41,31 @@ int main(void) {
     printf("[INF] I2C register: %d\n", ret);
   }
 
-  pthread_t thread_sensor;
+  pthread_t thread_adc;
+  pthread_t thread_accel;
   pthread_t thread_socket;
+  pthread_t thread_display;
 
   payload_adc.id = ID_WATERLEVEL;
   payload_accel.id = ID_ACCEL;
 
-  if (pthread_create(&thread_sensor, NULL, thread_job_adc, NULL) < 0) {
+  // PCF8561 ADC measurement thread
+  if (pthread_create(&thread_adc, NULL, thread_job_adc, NULL) < 0) {
     goto thread_fail;
   }
 
-  if (pthread_create(&thread_sensor, NULL, thread_job_accel, NULL) < 0) {
+  // ADXL345 acceleromter measurement thread
+  if (pthread_create(&thread_accel, NULL, thread_job_accel, NULL) < 0) {
     goto thread_fail;
   }
 
+  // socket communication thread
   if (pthread_create(&thread_socket, NULL, thread_job_socket, NULL) < 0) {
+    goto thread_fail;
+  }
+
+  // LED display thread
+  if (pthread_create(&thread_display, NULL, thread_job_display, NULL) < 0) {
     goto thread_fail;
   }
 
@@ -117,9 +130,9 @@ void *thread_job_accel(void *arg) {
     if (ret < 0) {
       printf("[ERR] ADXL345 read failed: %s\n", strerror(-ret));
     } else {
-      // TODO: fine tuning data range required
-      payload_accel.note = data.x;
-      payload_accel.volume = data.y;
+      // filter positive G and fit to data range
+      payload_accel.note = (int)((data.z < 0.0) ? (data.z * -1.0) : 0.0) * 20;
+      payload_accel.volume = 80; // fixed volume
     }
 
     usleep(100000);
@@ -134,7 +147,6 @@ void *thread_job_socket(void *arg) {
   printf("[SOCKET] server: %s\n", SERVER_IP);
   printf("[SOCKET] initiating socket...\n");
 
-  int ret;
   int sock = socket(AF_INET, SOCK_STREAM, 0);
 
   if (sock < 0) {
@@ -145,7 +157,7 @@ void *thread_job_socket(void *arg) {
   init_socket_server(&server, SERVER_IP, SERVER_PORT);
 
   printf("[SOCKET] waiting for server...\n");
-  ret = connect(sock, (struct sockaddr*)&server, sizeof(server));
+  int ret = connect(sock, (struct sockaddr*)&server, sizeof(server));
 
   if (ret < 0) {
     goto socket_fail;
@@ -159,7 +171,7 @@ void *thread_job_socket(void *arg) {
       printf("[SOCKET] write failed: %s\n", strerror(errno));
     }
 
-    printf("[SERVER] write(%d): { id: %d, note: %d, volume: %d }\n", ret, payload_adc.id, payload_adc.note, payload_adc.volume);
+    printf("[SOCKET] write(%d): { id: %d, note: %d, volume: %d }\n", ret, payload_adc.id, payload_adc.note, payload_adc.volume);
 
     /* acceleromter value transmission */
     ret = write(sock, &payload_accel, sizeof(payload_t));
@@ -168,12 +180,53 @@ void *thread_job_socket(void *arg) {
       printf("[SOCKET] write failed: %s\n", strerror(errno));
     }
 
-    printf("[SERVER] write(%d): { id: %d, note: %d, volume: %d }\n", ret, payload_accel.id, payload_accel.note, payload_accel.volume);
+    printf("[SOCKET] write(%d): { id: %d, note: %d, volume: %d }\n", ret, payload_accel.id, payload_accel.note, payload_accel.volume);
 
-    usleep(100000);
+    /* read and draw display graph */
+    ret = read(sock, display_data, sizeof(display_data));
+    ledmatrix_drawgraph(display_data, 4);
+
+    usleep(100000); // transmission delay 100ms
   }
 
 socket_fail:
   printf("[SOCKET] socket connection failed\n");
+  pthread_exit((void *) -1);
+}
+
+
+/**************************************************************************
+ * read data from server and draw on the 160*16 LED display
+ */
+void *thread_job_display(void *arg) {
+  printf("[   LED] server: %s\n", SERVER_IP);
+  printf("[   LED] initiating socket...\n");
+
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (sock < 0) {
+    goto socket_fail;
+  }
+
+  struct sockaddr_in server;
+  init_socket_server(&server, SERVER_IP, SERVER_PORT + 1);
+
+  printf("[   LED] waiting for server...\n");
+  int ret = connect(sock, (struct sockaddr*)&server, sizeof(server));
+
+  if (ret < 0) {
+    goto socket_fail;
+  }
+
+  while (1) {
+    /* read and draw display graph */
+    ret = read(sock, display_data, sizeof(display_data));
+    ledmatrix_drawgraph(display_data, 4);
+
+    usleep(20000); // 50Hz update
+  }
+
+socket_fail:
+  printf("[   LED] socket connection failed\n");
   pthread_exit((void *) -1);
 }
