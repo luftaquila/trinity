5fe5b62c168fce93beab2982fa3759352884d868
anak <ilovekdh1208@ajou.ac.kr>
2023-12-06 22:22:50 +0900

    fix: 초음파 센서 간헐적 멈춤 현상 수정

--- rpi-sensor-ultrasonic/main.c
+++ rpi-sensor-ultrasonic/main.c
@@ -13,7 +13,7 @@
 
 #include "drivers/gpio.h"
 
-// #define SERVER_MOCK 1
+#define SERVER_MOCK 1
 #include "types.h"
 
 #define POUT 23
@@ -22,10 +22,9 @@
 #define PIN2 6
 #define REPEAT_MAX 5
 
-pthread_mutex_t decay_number_lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t result_lock = PTHREAD_MUTEX_INITIALIZER;
 
 payload_t result;
-double filterValue = 0;
 
 void *thread_job_sensor(void *arg);
 void *thread_job_sensor2(void *arg);
@@ -33,17 +32,18 @@ void *thread_job_socket(void *arg);
 
 int main(int argc, char *argv[])
 {
-  pthread_t thread_sensor;
+  pthread_t thread_sensor_note;
+  pthread_t thread_sensor_volume;
   pthread_t thread_socket;
 
   printf("complete\n");
   result.id = ID_ULTRASONIC;
 
-  if (pthread_create(&thread_sensor, NULL, thread_job_sensor, NULL) < 0)
+  if (pthread_create(&thread_sensor_note, NULL, thread_job_sensor, NULL) < 0)
   {
     printf("thread_job_sensor failed\n");
   }
-  if (pthread_create(&thread_sensor, NULL, thread_job_sensor2, NULL) < 0)
+  if (pthread_create(&thread_sensor_volume, NULL, thread_job_sensor2, NULL) < 0)
   {
     printf("second thread_job_sensor failed\n");
   }
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
     printf("thread_socket failed\n");
   }
 
-  pthread_join(thread_sensor, NULL);
+  pthread_join(thread_sensor_volume, NULL);
 
   return (0);
 }
@@ -61,7 +61,6 @@ void *thread_job_sensor(void *arg)
 {
   clock_t start_t, end_t;
   double time;
-  int repeat1 = 0;
 
   printf("[SENSOR] collecting sensor data1...\n");
 
@@ -70,7 +69,6 @@ void *thread_job_sensor(void *arg)
     printf("gpio export err\n");
     goto job_fail;
   }
-
   usleep(20000);
 
   if (-1 == GPIODirection(POUT, OUT) || -1 == GPIODirection(PIN, IN))
@@ -90,7 +88,7 @@ void *thread_job_sensor(void *arg)
       goto job_fail;
     }
 
-    usleep(10);
+    usleep(100);
     GPIOWrite(POUT, 0);
 
     while (GPIORead(PIN) == 0)
@@ -102,25 +100,24 @@ void *thread_job_sensor(void *arg)
       end_t = clock();
     }
 
-    time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // ms
+    time = (double)(end_t - start_t) / CLOCKS_PER_SEC;
     double distance = (time / 2) * 34000;
 
-    filterValue += distance;
-
-    repeat1++;
-
-    if (repeat1 == REPEAT_MAX)
+    if (distance <= 150 && distance >= 40)
     {
-      int val = ((int)filterValue / 5);
-      if (val <= 150)
-      {
-        result.note = val;
-      }
-      printf("result note: %d\n", result.note);
-      filterValue = 0;
-      repeat1 = 0;
+      result.note = (int)distance;
     }
-    usleep(20000);
+    else if (distance < 40)
+    {
+      result.note = 40;
+    }
+    else
+    {
+      result.note = 150;
+    }
+    printf("result note: %d\n", result.note);
+
+    usleep(50000);
   } while (1);
 
   if (-1 == GPIOUnexport(POUT) || -1 == GPIOUnexport(PIN))
@@ -138,7 +135,6 @@ void *thread_job_sensor2(void *arg)
   {
     clock_t start_t, end_t;
     double time;
-    int repeat2 = 0;
 
     printf("[SENSOR] collecting sensor data2...\n");
 
@@ -147,7 +143,7 @@ void *thread_job_sensor2(void *arg)
       printf("gpio export err\n");
       goto job_fail;
     }
-    usleep(100000);
+    usleep(20000);
 
     if (-1 == GPIODirection(POUT2, OUT) || -1 == GPIODirection(PIN2, IN))
     {
@@ -156,7 +152,7 @@ void *thread_job_sensor2(void *arg)
     }
 
     GPIOWrite(POUT2, 0);
-    usleep(100000);
+    usleep(20000);
 
     do
     {
@@ -166,7 +162,7 @@ void *thread_job_sensor2(void *arg)
         goto job_fail;
       }
 
-      usleep(10);
+      usleep(100);
       GPIOWrite(POUT2, 0);
 
       while (GPIORead(PIN2) == 0)
@@ -180,22 +176,20 @@ void *thread_job_sensor2(void *arg)
 
       time = (double)(end_t - start_t) / CLOCKS_PER_SEC; // ms
       double distance = (time / 2) * 34000;
-
-      filterValue += distance;
-
-      repeat2++;
-
-      if (repeat2 == REPEAT_MAX)
+      if (distance <= 100 && distance >= 40)
       {
-        int val = (int)filterValue / 5;
-        if (val <= 100)
-        {
-          result.volume = val;
-        }
-        printf("result volume: %d\n", result.volume);
-        filterValue = 0;
-        repeat2 = 0;
+        result.volume = (int)distance;
       }
+      else if (distance < 40)
+      {
+        result.volume = 40;
+      }
+      else
+      {
+        result.volume = 100;
+      }
+      printf("result volume: %d\n", result.volume);
+
       usleep(100000);
     } while (1);
 
